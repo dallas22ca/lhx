9fabf14049d648e55fd6896d8612a5bf665fd717
Dallas Read <dallas@excitereative.ca>
2014-02-25 16:48:06 -0400

    Init commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+### API and JS App
+
+### Required Features
+
+1. Install WP Plugin / Visit site
+2. Sign up
+3. Add domain
+4. Can change domain
+5. Get embed code / Code is added to site
+6. Can buy addons (hosted Stripe form)
+   - SMS ($15 / month)
+   - Triggers ($5 / month)
+   - Surveys ($5 / month)
+   - Multi-agent ($5 / agent / month)
+   - Cobrowsing ($25 / month)
+7. Ticketing system
+8. Offline lead collection
+9. Mobile Chatting with visitors
+10. Require Name and Email Address
+11. Cross-browser sound
+
+### When a user wants to chat:
+
+1. They click on the Call To Action
+2. New Window opens and secure session starts
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lhx::Application.config.secret_key_base = 'ba482118378a5cc36cd87abb5cb5753b61500caedf578cd098eb956e84ad897a5d2cd0b8e060d8e272b477173ac13e9c18d002e81d4b706d92f085c2bea11ba9'
