6b7e4511027e6c8a54673b92588ab7b1dc04e9b2
Dallas Read <dallas@excitereative.ca>
2014-02-25 17:00:09 -0400

    Agents and accounts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Lhx::Application.routes.draw do
+  devise_for :agents
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
